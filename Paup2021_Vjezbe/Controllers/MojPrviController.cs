3c9b7f9c527ca0de7a98a5c9ca93f4cdbf398650
mev <mev@PC-19-12>
2021-03-23 16:52:46 +0100

    Add project files.

---
+++ Paup2021_Vjezbe/Controllers/MojPrviController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup2021_Vjezbe.Controllers
+{
+    public class MojPrviController : Controller
+    {
+        // GET: MojPrvi
+        public ActionResult Index()
+        {
+            ViewBag.Lokacija = "Međimursko veleučilište u Čakovcu";
+            return View();
+        }
+
+        public ActionResult Druga()
+        {
+            ViewBag.Ustanova = "Međimursko veleučilište";
+            ViewData["Lokacija"] = "Čakovec";
+            return View();
+        }
+
+        public ActionResult Treca(string poruka, int broj=1)
+        {
+            ViewBag.Poruka = poruka;
+            ViewBag.Broj = broj;
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+            ViewBag.Ime = "Sanja";
+            ViewBag.Prezime = "Perić";
+            ViewBag.GodinaRodjenja = 1995;
+
+            return View();
+        }
+
+        public string VratiVrijeme()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        public string VratiVrijemeUtorak()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        //Merge 14.03.2021 10:55
+        public string PericaHuten()
+        {
+            return "Perica Huten";
+        }
+
+        //Merge 14.03.2021 10:53
+        public string CarKarlo()
+        {
+            return "Karlo Car";
+        } 
+        public string JovanovicMatija()
+        {
+            return "Jovanović Matija";
+        }
+        public string NovakLucija()
+        {
+            return "Novak Lucija";
+        }
+        public string BesenicErik()
+        {
+            return "Besenic Erik";
+        }
+    }
+}
---
+++ Paup2021_Vjezbe/Controllers/StudentiController.cs
@@ -0,0 +1,59 @@
+﻿using Paup2021_Vjezbe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup2021_Vjezbe.Controllers
+{
+    public class StudentiController : Controller
+    {
+        // GET: Studenti
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Početna stranica o studentima";
+            ViewBag.Fakultet = "Međimursko veleučilište";
+            return View();
+        }
+
+        public ActionResult Popis()
+        {
+            //Instanciramo klasu StudentiDB koja sadržava listu studenata
+            StudentiDB studenti = new StudentiDB();
+            //Objekt studentidb klase StudentiDB prosljeđujemo u View kao njegov model
+            return View(studenti);
+        }
+
+        //int? definiramo da parametar id može biti nullabilan, tj da nemora biti definirana njegova vrijednost
+        public ActionResult Detalji(int? id)
+        {
+            //provjeravamo ako parametar id nema vrijednost, tj on nije definiran
+            if (!id.HasValue)//ili if (id.HasValue == false)
+            {
+                //preusmjeravamo korisnika na akciju Popis
+                return RedirectToAction("Popis");
+            }
+
+            //Instanciramo klasu StudentiDB koja sadržava listu studenata
+            StudentiDB studenti = new StudentiDB();
+
+            /*
+               * sa objekta studentidb pozivamo metodu VratiListu() koja nam vraća listu studenata
+               * pomoću Lambda izraza FirstOrDefault(x => x.Id == id) dohvaćamo prvog elementa iz liste
+               * kojemu se vrijednost propertya Id podudara sa vrijednošću parametra id
+               */
+            Student student = studenti.VratiListu().FirstOrDefault(x => x.Id == id);
+
+            //ako u listi nema studenta sa traženim Id-em onda je varijabla student null
+            if (student == null)
+            {
+                //u tom slučaju preusmjeravamo korisnika na akciju Popis
+                return RedirectToAction("Popis");
+            }
+
+            //Objekt student klase Student prosljeđujemo u View kao njegov model
+            return View(student);
+        }
+    }
+}
